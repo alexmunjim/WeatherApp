00751592d35d3399d119f9ca552ff19dbdb72bb4
alexmunjim <52839109+alexmunjim@users.noreply.github.com>
2024-08-07 18:08:41 -0500

    Creación del proyecto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-slate-500 text-2x5 flex'>Mi primera aplicacion del clima</h1>
+    </>  
+  );
+}
+
+export default App;
