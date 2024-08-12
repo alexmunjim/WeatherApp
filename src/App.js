2bf551e7702803e46d06cee8c8911dd6595aa19e
Fernando Muñoz <79942967+fermumu@users.noreply.github.com>
2024-08-12 13:08:23 -0500

    commit-8
    modificacion archivo app.js para que no cree conflicto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Prueba from './components/Prueba';
 
 function App() {
   return (
     <>
       <h1 className='bg-slate-500 text-2x5 flex'>Mi primera aplicacion del clima</h1>
-      <Prueba/>
     </>  
   );
 }
